2d7e34d80ee1f9176c3c43802a095ff5546a0cb9
TohaSKA <spacantonio@gmail.com>
2023-05-28 17:28:55 +0300

    Добавьте файлы проекта.

---
+++ Shop/Controllers/CarsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.interfaces;
+using Shop.Data.Models;
+using Shop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Shop.Controllers
+{
+    public class CarsController : Controller {
+
+        private readonly IALLCars _allCars;
+        private readonly ICarsCategory _allCategories;
+
+        public CarsController(IALLCars iALLCars, ICarsCategory iCarsCat)
+        {
+            _allCars = iALLCars;
+            _allCategories = iCarsCat;
+        }
+        [Route("Cars/List")]
+        [Route("Cars/List/{Category}")]
+        public  ViewResult List (string category)
+        {
+            string _category = category;
+            IEnumerable<Car> cars = null;
+            string currCategory = "";
+            if (string.IsNullOrEmpty(category))
+            {
+                cars = _allCars.Cars.OrderBy(i => i.id);
+            }
+            else
+            {
+                if (string.Equals("limbi de studiere mai complicata", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.categoryName.Equals("Cursuri populare")).OrderBy(i => i.id);
+                    currCategory = "Cursuri populare";
+                }
+                else if (string.Equals("limbi de studiere mai simpla", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.categoryName.Equals("Cursuri mai putin populare")).OrderBy(i => i.id);
+                    currCategory = "Cursuri mai putin populare";
+
+                }
+
+                currCategory = _category;
+
+                
+            }
+            var carObj = new CarsListViewModel
+            {
+                allCars = cars,
+                currCategory = currCategory
+            };
+
+
+
+
+            ViewBag.Title = "Pagina cu cursuri";
+            
+            return View(carObj);
+        }
+    }
+}
---
+++ Shop/Data/mocks/MockCategory.cs
@@ -0,0 +1,24 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.mocks
+{
+    public class MockCategory : ICarsCategory
+    {
+        public IEnumerable<Category> ALLCategories
+        {
+            get
+            {
+                return new List<Category>
+                {
+                    new Category { categoryName = "Cursuri populare", desc = "Cele ma popullare cursuri in Moldova"},
+                    new Category { categoryName = "Cursuri mai putin populare", desc = "Cursurile ce nu dispun de o popularitate inalta"}
+                }; 
+            }
+        }
+    }
+}
